a2e05f03008e762b0b14d4ac2458231992a59635
David Cumps <david@cumps.be>
2018-06-18 01:38:00 +0200

    feat: add omf to dotfiles

---
+++ fish/init.fish
@@ -0,0 +1,9 @@
+# Bobthefish theme settings
+set -g theme_title_display_process yes
+set -g theme_nerd_fonts yes
+set -g theme_color_scheme solarized-dark
+set -g theme_date_format "+%a %F // %R"
+set -g theme_display_ruby no
+set -g theme_display_user ssh
+set -g theme_display_hostname ssh
+
