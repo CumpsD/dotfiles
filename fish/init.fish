992bc5b96df2bd57b1ef417a145921e91812249a
David Cumps <david@cumps.be>
2018-06-18 02:15:04 +0200

    feat: configure DISPLAY

--- fish/init.fish
+++ fish/init.fish
@@ -1,6 +1,9 @@
 # Note: Bash on Windows does not currently apply umask properly.
 if [ (umask) = "0000" ]; umask 022; end
 
+# Export our Display to get it into Windows
+set -gx DISPLAY :0
+
 # Set a default editor
 set -gx EDITOR nano
 
