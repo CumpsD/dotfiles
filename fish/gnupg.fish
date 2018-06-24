a8f63dd6e18b67b4648206cada13b23480e3e3a6
David Cumps <david@cumps.be>
2018-06-24 22:40:09 +0200

    fix: get rid of 'OK' on start

--- fish/gnupg.fish
+++ fish/gnupg.fish
@@ -4,4 +4,4 @@ gpgconf --launch gpg-agent
 set -gx SSH_AUTH_SOCK ~/.gnupg/S.gpg-agent.ssh
 set -gx GPG_TTY (tty)
 
-gpg-connect-agent updatestartuptty /bye
+gpg-connect-agent updatestartuptty /bye > /dev/null
