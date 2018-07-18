eaf6097dd0603b0367c3cec4459dc1293116b9ca
David Cumps <david@cumps.be>
2018-07-18 13:31:59 +0200

    feat: add a check to only execute nuget-nuke in a repo

--- fish/functions/nuke-nuget.fish
+++ fish/functions/nuke-nuget.fish
@@ -1,4 +1,18 @@
 function nuke-nuget
-        rm -rf packages; .paket/paket.exe clear-cache; dotnet nuget locals all --clear; rm -rf ~/.nuget/
+	if pwd | grep '/dotfiles/repos' > /dev/null
+	        echo 'Removing packages...'
+		rm -rf packages
+
+		echo 'Clearing Paket cache...'
+		mono .paket/paket.exe clear-cache
+
+		echo 'Clearing NuGet cache...'
+		dotnet nuget locals all --clear
+
+		echo 'Removing NuGet...'
+		rm -rf ~/.nuget/
+	else
+		echo 'WARNING: Not in a dotfiles/repos folder'
+	end
 end
 
