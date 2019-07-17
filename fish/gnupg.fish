5494756afdf614e6f277a85db55d0434d6765465
David Cumps <david@cumps.be>
2019-07-17 23:58:11 +0200

    fix: make gpg agent work in arch and wsl

--- fish/gnupg.fish
+++ fish/gnupg.fish
@@ -1,7 +1,15 @@
 # Start or re-use a gpg-agent.
 
 gpgconf --launch gpg-agent
-set -gx SSH_AUTH_SOCK ~/.gnupg/S.gpg-agent.ssh
+
+if test -e ~/.gnupg/S.gpg-agent.ssh
+    set -gx SSH_AUTH_SOCK ~/.gnupg/S.gpg-agent.ssh
+end
+
+if test -e /run/user/1000/gnupg/S.gpg-agent.ssh
+    set -gx SSH_AUTH_SOCK /run/user/1000/gnupg/S.gpg-agent.ssh
+end
+
 set -gx GPG_TTY (tty)
 
 gpg-connect-agent updatestartuptty /bye > /dev/null
