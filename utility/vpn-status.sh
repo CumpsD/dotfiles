9bb54659270b87eb94d57ab5d7be1e5c1a908ac9
David Cumps <david@cumps.be>
2019-08-02 00:36:53 +0200

    refactor: vpn disconnect in color

--- utility/vpn-status.sh
+++ utility/vpn-status.sh
@@ -5,5 +5,5 @@ STATUS=$(pgrep -a 'openvpn$' | head -n 1 | sed -r 's/.*nm-openvpn\/(.*)-ca.pem.*
 whitespace=$(printf '\n\t ')
 case "$STATUS" in
   *[!$whitespace]*) echo "$STATUS";;
-  *) echo "Disconnected";;
+  *) echo "%{F#787e68}Disconnected%{F-}";;
 esac
